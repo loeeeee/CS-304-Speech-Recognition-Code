5132a90c1d40b93efd0ed0e8d611d66e25dab645
loeeeee <95266635+loeeeee@users.noreply.github.com>
2025-01-15 21:33:58 +0800

    complete routine function

--- src/loe-speech-recognition/segmentation.py
+++ src/loe-speech-recognition/segmentation.py
@@ -1,10 +1,16 @@
 import queue
 import sys
-from typing import List, Literal, Self
+from typing import List, Literal, Self, no_type_check
 from dataclasses import dataclass, field
+import itertools
+import os
+import wave
+import logging
 
 import sounddevice as sd
 import numpy as np
+
+logger = logging.getLogger(__name__)
 
 @dataclass
 class Segmentation:
@@ -18,30 +24,67 @@ class Segmentation:
 
     # Internals
     _noise_floor: int = field(default=0) # May use default factory for an init measurement
+    _speech_started: bool = field(default=False)
+    _speech_ended: bool = field(default=False)
+    _results: List[np.ndarray] = field(default_factory=list)
+    _per_frame_time: float = field(default=0.02) # Initialize this when main is called
 
-    def write_to_wave(self) -> None:
-        ...
+    @no_type_check
+    def write_to_wave(self, frames: np.ndarray, name: str) -> None:
+        path = os.path.join("segment_results", f"{name}.wav")
+        with wave.open(path, "wb") as wav:
+            wav.setframerate(self.stream.samplerate)
+            wav.setnchannels(self.stream.channels)
+            wav.setsampwidth(2) # 16 bit
+            wav.writeframes(frames.tobytes())
+
+        logger.debug(f"Save to {name}.wav")
+        return
 
     def main(self) -> None:
         """The main loop where hit-to-talk happens"""
-        try:
-            while True:
-                try:
-                    input("Press any key to start recording")
-                except KeyboardInterrupt:
-                    print("CTRL-C also starts recording")
-                
-                # Routine
+        self._per_frame_time = 1 / self.stream.samplerate * self.frame_size
 
-                pass 
+        try:
+            with self.stream:
+                while True:
+                    input("Press any key to start recording")
+                    
+                    # Routine
+
+                    pass 
         except KeyboardInterrupt:
-            print("Gracefully exit")
+            print("Gracefully exiting")
+            for index, segment in enumerate(self._results):
+                self.write_to_wave(segment, str(index).zfill(2))
         ...
 
     def routine(self) -> None:
         """Actually do things here"""
-        
-        ...
+        # Get all cached signal
+        audio = self.get_all_frames_from_queue(self.audio_cache)
+
+        num_of_frames: int = audio.shape[0] // self.frame_size
+
+        # Construct an iterable for all audio
+        trimmed_audio = audio[:self.frame_size*num_of_frames]
+        iter_audio_frames = itertools.chain.from_iterable((trimmed_audio.reshape((-1, self.frame_size)), [audio[self.frame_size*num_of_frames:]]))
+
+        for frame in iter_audio_frames:
+            if self._speech_started:
+                # Detect speech continues
+                if self.detect_speech(frame, threshold="low"):
+                    self._results[-1] = np.concatenate((self._results[-1], frame))
+                    pass
+                else:
+                    self._speech_ended = True
+                    pass
+            else:
+                # Detect speech start
+                if self.detect_speech(frame, threshold="high"):
+                    self._results.append(frame) # Add new words to the results
+                    self._speech_started = True
+        return
 
     def detect_speech(self, frames: np.ndarray, threshold: Literal["high", "low"]) -> bool:
         # Detect Speech
