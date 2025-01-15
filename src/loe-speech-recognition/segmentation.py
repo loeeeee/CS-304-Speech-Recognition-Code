89c407aa898d8dba5c87bfc6d2034f63e2d77747
loeeeee <95266635+loeeeee@users.noreply.github.com>
2025-01-15 22:32:31 +0800

    redo noise floor

--- src/loe-speech-recognition/segmentation.py
+++ src/loe-speech-recognition/segmentation.py
@@ -6,11 +6,51 @@ import itertools
 import os
 import wave
 import logging
+import time
 
 import sounddevice as sd
 import numpy as np
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(filename='runtime.log', level=logging.DEBUG, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+@dataclass
+class NoiseFloor:
+    background_samples: List[np.ndarray] = field(default_factory=list)
+
+    # Settings
+    num_of_samples: int = field(default=5)
+
+    # Internal
+    _noise_floor: int = field(default=0)
+
+    def update_noise_floor(self, samples: np.ndarray) -> int:
+        self.background_samples.append(samples)
+        if len(self.background_samples) > self.num_of_samples:
+            self.background_samples.pop(0)
+        
+        self._calculate_noise_floor()
+
+        return self._noise_floor
+
+    def _calculate_noise_floor(self) -> None:
+        noise_floor = 0
+        multiplier: int = 0
+        for index, samples in enumerate(reversed(self.background_samples)):
+            multiplier += self.num_of_samples - index
+            noise_floor += (self.num_of_samples - index) * np.average(np.abs(samples))
+
+        self._noise_floor: int = int(noise_floor / multiplier)
+        logger.debug(f"Current noise floor is {noise_floor}")
+        return
+
+    def __str__(self) -> str:
+        return str(self._noise_floor)
+
+    @property
+    def noise_floor(self) -> int:
+        return self._noise_floor
 
 @dataclass
 class Segmentation:
@@ -23,22 +63,23 @@ class Segmentation:
     speech_low_threshold: int = field(default=64) # Cut volume
 
     # Internals
-    _noise_floor: int = field(default=0) # May use default factory for an init measurement
+    _noise_floor: NoiseFloor = field(default_factory=NoiseFloor) # May use default factory for an init measurement
     _speech_started: bool = field(default=False)
     _speech_ended: bool = field(default=True)
     _results: List[np.ndarray] = field(default_factory=list)
     _per_frame_time: float = field(default=0.02) # Initialize this when main is called
 
     @no_type_check
-    def write_to_wave(self, frames: np.ndarray, name: str) -> None:
+    def write_to_wave(self, samples: np.ndarray, name: str) -> None:
         path = os.path.join("segment_results", f"{name}.wav")
+        sample_rate: int = self.stream.samplerate
         with wave.open(path, "wb") as wav:
-            wav.setframerate(self.stream.samplerate)
+            wav.setframerate(sample_rate)
             wav.setnchannels(self.stream.channels)
             wav.setsampwidth(2) # 16 bit
-            wav.writeframes(frames.tobytes())
+            wav.writeframes(samples.tobytes())
 
-        logger.debug(f"Save to {name}.wav")
+        logger.debug(f"Save {samples.shape[0] * self._per_frame_time / self.frame_size:.2f}s audio to {name}.wav")
         return
 
     def main(self) -> None:
@@ -47,14 +88,18 @@ class Segmentation:
 
         try:
             with self.stream:
+                logger.debug("Entering recording stream")
+                input("Press any key to start recording")
+                self.initialize_noise_floor()
                 while True:
-                    input("Press any key to start recording")
+                    logger.debug("Start recording")
 
                     # Routine
                     self.routine()
+                    time.sleep(0.2) # Don't draw too much CPU
 
         except KeyboardInterrupt:
-            print("Gracefully exiting")
+            print("\nGracefully exiting")
             for index, segment in enumerate(self._results):
                 self.write_to_wave(segment, str(index).zfill(2))
         ...
@@ -88,12 +133,15 @@ class Segmentation:
                     self._speech_started = True
                     self._speech_ended = False
                     logger.info("Speech recognized")
+                else:
+                    # Update noise floor
+                    self._noise_floor.update_noise_floor(frame)
         return
 
     def detect_speech(self, frames: np.ndarray, threshold: Literal["high", "low"]) -> bool:
         # Detect Speech
-        abs_total_frame = np.abs(frames) - self._noise_floor
-        average_energy = np.average(abs_total_frame)
+        total_energy = np.abs(frames) - self._noise_floor.noise_floor # Subtract noise floor for more consistent performance
+        average_energy = np.average(total_energy)
 
         if threshold == "high" and average_energy > self.speech_high_threshold:
                 return True
@@ -102,13 +150,12 @@ class Segmentation:
 
         return False
 
-    # ---------------
-    @staticmethod
-    def get_noise_floor(frames: np.ndarray) -> int:
-        noise_floor: int = int(np.average(np.abs(frames)))
-        logger.debug(f"Current noise floor is {noise_floor}")
-        return noise_floor
+    def initialize_noise_floor(self) -> None:
+        samples = self.get_all_frames_from_queue(self.audio_cache)
+        self._noise_floor.update_noise_floor(samples)
+        logger.info(f"Noise floor is initialized to {self._noise_floor}")
 
+    # ---------------
     @staticmethod
     def get_all_frames_from_queue(cache: queue.Queue) -> np.ndarray:
         results: np.ndarray = cache.get().reshape(-1)
@@ -145,8 +192,13 @@ class Segmentation:
 
 
 def main() -> None:
-
-
     seg = Segmentation.from_basic(
         sample_rate=16000
     )
+    seg.speech_high_threshold = 256
+    seg.speech_low_threshold = 128
+    seg.main()
+
+
+if __name__ == "__main__":
+    main()
