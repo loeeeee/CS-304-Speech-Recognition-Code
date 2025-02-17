3976e15fdf0299be90af595cfcc62c718f1afbfe
loeeeee <95266635+loeeeee@users.noreply.github.com>
2025-02-17 12:51:58 +0800

    add new things

--- src/loe_speech_recognition/__init__.py
+++ src/loe_speech_recognition/__init__.py
@@ -2,11 +2,14 @@ from .mfcc import MFCC
 from .segmentation import Segmentation
 from .dynamic_time_wrapping import DynamicTimeWarping
 from .ti_digits import TIDigits, DataLoader
+from .hidden_markov_model import HiddenMarkovModel, SortedSignal
 
 __all__ = [
     "MFCC",
     "Segmentation",
     "DynamicTimeWarping",
     "TIDigits",
-    "DataLoader"
+    "DataLoader",
+    "HiddenMarkovModel",
+    "SignalSegment",
 ]
