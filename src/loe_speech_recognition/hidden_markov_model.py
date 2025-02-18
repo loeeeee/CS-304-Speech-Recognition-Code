23fc00cff00bf41e048d60c712086376d7ba0c5d
loeeeee <95266635+loeeeee@users.noreply.github.com>
2025-02-18 23:46:40 +0800

    more objects

--- src/loe_speech_recognition/hidden_markov_model.py
+++ src/loe_speech_recognition/hidden_markov_model.py
@@ -8,7 +8,7 @@ import scipy as sp
 logger = logging.getLogger(__name__)
 
 @dataclass
-class SortedSignal:
+class Signal:
     # Mains
     num_of_state: int
     signal: np.ndarray
@@ -45,6 +45,105 @@ class SortedSignal:
     def order_by_signal(self) -> List[Tuple[np.ndarray, int]]:
         return [(signal, state) for signal, state in zip(self.signal, self.path)]
 
+@dataclass
+class SortedSignals:
+    # Mains
+    num_of_states: int
+
+    # Internals
+    _signals: List[Signal] = field(init=False)
+
+    def __post_init__(self) -> None:
+        self._signals = []
+        return
+
+    def append(self, signal: Signal) -> None:
+        self._signals.append(signal)
+
+    @property
+    def order_by_state(self) -> List[List[np.ndarray]]:
+        signals_by_state: List[List[np.ndarray]] = [[] for _ in range(self.num_of_states)]
+        for signal in self._signals:
+            logger.info(f"Next signal")
+            for state, signal in enumerate(signal.order_by_state):
+                logger.debug(f"state: {state}")
+                if not signal is None:
+                    # Skip when some state has no signal
+                    logger.debug(f"signal shape: {signal.shape}")
+                    signals_by_state[state].append(signal)
+        return signals_by_state
+
+    @property
+    def transition_probabilities(self) -> np.ndarray:
+        transition_counts: np.ndarray = np.zeros((self.num_of_states, self.num_of_states))
+        for signal in self._signals:
+            last_state: int = signal.path[0]
+            for current_state in signal.path[1:]:
+                transition_counts[last_state: current_state] += 1
+                last_state = current_state
+        transition_probs = transition_counts / np.sum(transition_counts, axis=1, keepdims=True)
+        return transition_probs
+
+class HMMTrainMeanFail(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+        logger.warning(f"Cannot use all the state for the HMM")
+
+
+class HMMTrainConverge(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+        logger.warning("Successfully train the HMM model")
+
+@dataclass
+class HiddenMarkovModelInitializer:
+    # Mains
+    num_of_states: int
+    train_data: List[np.ndarray]
+    dim_of_feature: int = field(default=39)
+
+    # Internals
+    _init_seed: int = field(default=0)
+
+    def __post_init__(self) -> None:
+        ## Set up dimension
+        logger.info(f"Created HMM initializer")
+        return
+
+    def init_values(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        # Transition Probabilities
+        transition_probabilities: List[List[float]] = []
+        for current_state_index in range(self.num_of_states):
+            inner_list: List[float] = [0. for _ in range(current_state_index)]
+            inner_list.extend([1/(self.num_of_states - current_state_index) for _ in range(self.num_of_states - current_state_index)])
+            transition_probabilities.append(inner_list)
+        transition_probs = np.array(transition_probabilities)
+        logger.debug("Finish compute transition probabilities")
+
+        # Means
+        means: List[float] = [0. for _ in range(self.num_of_states)]
+        first_signal = self.train_data[self._init_seed]
+        logger.info(f"First signal has shape {first_signal.shape}")
+        state_length: int = int(first_signal.shape[0] / self.num_of_states) # Length of the time series
+        for state_index in range(self.num_of_states):
+            start_position: int = state_index * state_length
+            signal_slice = first_signal[start_position: start_position+state_length, :]
+            # Mean (dim_of_feature, )
+            mean = np.average(signal_slice, axis=0)
+            means[state_index] = mean
+        logger.debug("Finish compute means")
+
+        # Covariance
+        covariance = np.tile(np.eye(self.dim_of_feature), (self.num_of_states, 1, 1)) * 0.01
+
+        # Init seed
+        self._init_seed += 1
+        return transition_probs, np.array(means), covariance
+
+    @property
+    def init_counter(self) -> int:
+        return self._init_seed
+
 
 @dataclass
 class HiddenMarkovModel:
@@ -57,6 +156,7 @@ class HiddenMarkovModel:
     _transition_prob: np.ndarray = field(init=False)
     _means: np.ndarray = field(init=False)
     _covariances: np.ndarray = field(init=False)
+    _initializer: HiddenMarkovModelInitializer = field(init=False)
 
     def __post_init__(self) -> None:
         # Init all parameters
@@ -87,31 +187,11 @@ class HiddenMarkovModel:
         ## | 0     0.5   0.5   |
         ## | 0     0     1     |
 
-        transition_probabilities: List[List[float]] = []
-        for current_state_index in range(num_of_states):
-            inner_list: List[float] = [0. for _ in range(current_state_index)]
-            inner_list.extend([1/(num_of_states - current_state_index) for _ in range(num_of_states - current_state_index)])
-            transition_probabilities.append(inner_list)
-        ## Assign the calculation result to hmm
-        hmm._transition_prob = np.array(transition_probabilities)
-
-        # Segmental K-means
-        ## Create initial state
-        hmm._covariances = np.tile(np.eye(dim_of_feature), (num_of_states, 1, 1)) * 0.01  # Small diagonal covariances
-        
-        ## Initialize means
-        means: List[float] = [0. for _ in range(num_of_states)]
-        first_signal = train_data[0]
-        logger.info(f"First signal has shape {first_signal.shape}")
-        state_length: int = int(first_signal.shape[0] / num_of_states) # Length of the time series
-        for state_index in range(num_of_states):
-            start_position: int = state_index * state_length
-            signal_slice = first_signal[start_position: start_position+state_length, :]
-            # Mean (dim_of_feature, )
-            mean = np.average(signal_slice, axis=0)
-            means[state_index] = mean
-        hmm._means = np.array(means)
-        logger.debug("Finish compute means")
+        hmm._initializer = HiddenMarkovModelInitializer(
+            num_of_states=num_of_states,
+            train_data=train_data,
+            dim_of_feature=dim_of_feature
+        )
         
         for i in range(k_means_max_iteration):
             hmm.train(train_data=train_data)
@@ -122,36 +202,61 @@ class HiddenMarkovModel:
     def __str__(self) -> str:
         return self.label
 
-    def train(self, train_data) -> None:
-        # 2a. Segmentation (Viterbi):
-        sorted_signals: List[SortedSignal] = []
-        # segments: List[List[np.ndarray]] = []  # List to store segments for each sequence
+    def train(self, train_data, k_means_max_iteration: int = 100) -> None:
+        force_init: bool = True
+        for i in range(k_means_max_iteration):
+            if force_init:
+                self._transition_prob, self._means, self._covariances = self._initializer.init_values()
+                logger.debug("Init the three parameters")
+                force_init = False
+
+            try:
+                self.train_routine(train_data=train_data)
+            except HMMTrainMeanFail:
+                force_init = True
+                logger.warning(f"Reinit parameters next loop, {i+1}")
+            except HMMTrainConverge:
+                logger.info(f"🏁 Finish training at {i} iteration, after {self._initializer.init_counter} init attempt")
+                break
+
+        return
+
+    def train_routine_new(self, train_data) -> None:
+        # Segmentation
+        return
+
+
+    def train_routine(self, train_data) -> None:
+        # Segmentation
+        sorted_signals: SortedSignals = SortedSignals(self.num_of_states)
         for sequence in train_data:
             viterbi_path = self._viterbi(sequence, self.num_of_states, self._means, self._transition_prob, self._covariances)  # See function below
-            sorted_signals.append(SortedSignal(self.num_of_states, sequence, viterbi_path))
-            # segments.append(self._segment_data(sequence, viterbi_path)) # See function below
+            sorted_signals.append(Signal(self.num_of_states, sequence, viterbi_path))
 
         # Update parameters
+        
         ## Update means
-        signal_concat_by_state: List = [[] for _ in range(self.num_of_states)]
-        for sorted_signal in sorted_signals:
-            logger.info(f"Next signal")
-            for state, signal in enumerate(sorted_signal.order_by_state):
-                logger.debug(f"state: {state}")
-                if not signal is None:
-                    # Skip when some state has no signal
-                    logger.debug(f"signal shape: {signal.shape}")
-                    signal_concat_by_state[state].append(signal)
-        signal_concat_by_state = [np.concatenate(i) for i in signal_concat_by_state]
-        _average = [np.average(i, axis=0) for i in signal_concat_by_state]
-        logger.info(f"Average: {_average[0].shape}, {_average[1].shape}, {_average[2].shape}, {_average[3].shape}, {_average[4].shape}")
-        self._means = np.array(_average) # Calculate mean for all time series, keeping feature dimension
+        signals_sorted_by_state: List[List[np.ndarray]] = sorted_signals.order_by_state
+        try:
+            signal_concat_by_state = [np.concatenate(i) for i in signals_sorted_by_state]
+        except ValueError:
+            raise HMMTrainMeanFail
+        ### Do the update
+        new_means = [np.average(i, axis=0) for i in signal_concat_by_state]
+        logger.info(f"Average: {new_means[0].shape}, {new_means[1].shape}, {new_means[2].shape}, {new_means[3].shape}, {new_means[4].shape}")
+        if np.allclose(new_means, self._means):
+            logger.debug("Converges")
+            raise HMMTrainConverge
+        
+        self._means = np.array(new_means) # Calculate mean for all time series, keeping feature dimension
+
         ## Update covariance
         for state, signals in enumerate(signal_concat_by_state):
             logger.info(f"Local Segments has shape: {signals.shape}")
             self._covariances[state] = np.cov(signals, rowvar=False) + np.eye(self.dim_of_feature) * 0.001
+        
         ## Update transition probability
-        self._transition_prob = self._estimate_transition_prob(sorted_signals, self.num_of_states)
+        self._transition_prob = sorted_signals.transition_probabilities
 
     @staticmethod
     def _viterbi(observation_sequence: np.ndarray, num_of_states: int, means: List[float]|np.ndarray, transition_probs: np.ndarray, covariances: np.ndarray) -> List[int]:
@@ -189,15 +294,3 @@ class HiddenMarkovModel:
             viterbi_path[t] = psi[t+1, viterbi_path[t+1]]
 
         return viterbi_path # [np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(0), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(1), np.int64(2), np.int64(2), np.int64(2), np.int64(2), np.int64(2), np.int64(2), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(3), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4), np.int64(4)]
-
-    @staticmethod
-    def _estimate_transition_prob(sorted_signals: List[SortedSignal], num_of_state: int) -> np.ndarray:
-        transition_counts: np.ndarray = np.zeros((num_of_state, num_of_state))
-        for sorted_signal in sorted_signals:
-            last_state: int = sorted_signal.order_by_signal[0][1]
-            for _, current_state in sorted_signal.order_by_signal[1:]:
-                transition_counts[last_state: current_state] += 1
-                last_state = current_state
-
-        transition_probs = transition_counts / np.sum(transition_counts, axis=1, keepdims=True)
-        return transition_probs
