718a473e2723865857e16d4e9a38a2f3e834ff3b
loeeeee <95266635+loeeeee@users.noreply.github.com>
2025-02-20 05:54:11 +0800

    add confusion matrix plotting

--- scripts/project3_predict.py
+++ scripts/project3_predict.py
@@ -1,3 +1,4 @@
+from numpy.typing import NDArray
 from loe_speech_recognition import TIDigits, HiddenMarkovModel, MFCC, TI_DIGITS_LABELS, ModelCollection
 
 import logging
@@ -5,6 +6,7 @@ import concurrent.futures
 
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='./runtime.log', 
@@ -43,8 +45,31 @@ for index, label in enumerate(TI_DIGITS_LABELS):
     
     overall_bar.update()
 
+def plot_confusion_matrix(confusion_matrix: NDArray, name: str):
+    # Plot the confusion matrix
+    fig, ax = plt.subplots()
+    im = ax.imshow(confusion_matrix)
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(range(len(TI_DIGITS_LABELS)), labels=TI_DIGITS_LABELS,
+                rotation=45, ha="right", rotation_mode="anchor")
+    ax.set_yticks(range(len(TI_DIGITS_LABELS)), labels=TI_DIGITS_LABELS)
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(TI_DIGITS_LABELS)):
+        for j in range(len(TI_DIGITS_LABELS)):
+            text = ax.text(j, i, confusion_matrix[i, j],
+                        ha="center", va="center", color="w")
+
+    ax.set_title("Confusion matrix")
+    fig.tight_layout()
+    plt.savefig(f"./plots/confusion_matrix_{name}.png")
+
+
 logger.info(f"Confusion matrix when prediction on known data:")
 logger.info(f"{train_data_confusion_matrix}")
+plot_confusion_matrix(train_data_confusion_matrix, "known_data_small_model")
 logger.info(f"Confusion matrix when prediction on unknown data:")
 logger.info(f"{test_data_confusion_matrix}")
+plot_confusion_matrix(test_data_confusion_matrix, "unknown_data_small_model")
 
