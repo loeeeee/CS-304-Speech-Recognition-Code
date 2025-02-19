0501f272036d7d8f2c18d6a7780e284f6425f63f
loeeeee <95266635+loeeeee@users.noreply.github.com>
2025-02-19 23:50:08 +0800

    rename project3 script

--- scripts/project3_train.py
+++ scripts/project3_train.py
@@ -15,10 +15,13 @@ logger.info("Finish loading dataset")
 train_dataset = ti_digits.train_dataset
 
 logger.info("Start computing MFCCs")
-train_dataset_mfccs = [MFCC(i, sample_rate=16000).feature_vector.T for i in train_dataset["1"][:20]]
+train_dataset_mfccs = [MFCC(i, sample_rate=16000).feature_vector.T for i in train_dataset["1"][:10]]
 logger.info("Finish computing MFCCs")
 
-logging.getLogger().setLevel(logging.DEBUG)
+# logging.getLogger().setLevel(logging.DEBUG)
 logger.info("Start initialize HMM model from data")
 hmm = HiddenMarkovModel.from_data("0", 5, train_dataset_mfccs)
 logger.info("Finish initialize HMM model from data")
+
+logger.info("Start testing HMM model")
+logger.info("Finish testing HMM model")
