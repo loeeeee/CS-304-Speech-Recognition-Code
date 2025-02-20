7c7eeda7898e64b33073a9f54832a9a74fc6e231
loeeeee <95266635+loeeeee@users.noreply.github.com>
2025-02-20 10:15:46 +0800

    use big model by default

--- scripts/project3_predict.py
+++ scripts/project3_predict.py
@@ -18,15 +18,16 @@ ti_digits = TIDigits("./ConvertedTIDigits", isSingleDigits=True)
 logger.info("Finish loading dataset")
 
 train_dataset = ti_digits.train_dataset
+test_dataset = ti_digits.test_dataset
 
-mc = ModelCollection.load_from_files(".cache/small_model", 5, 39)
+mc = ModelCollection.load_from_files(".cache/big_model", 5, 39)
 
 train_data_confusion_matrix = np.zeros((len(TI_DIGITS_LABELS), len(TI_DIGITS_LABELS)))
 test_data_confusion_matrix = np.zeros((len(TI_DIGITS_LABELS), len(TI_DIGITS_LABELS)))
 overall_bar = tqdm(desc="Overall Progress", total=len(TI_DIGITS_LABELS), position=1)
 for index, label in enumerate(TI_DIGITS_LABELS):
     # Seen data
-    train_dataset_mfccs = [MFCC(i, sample_rate=16000).feature_vector.T for i in train_dataset[label][:10]]
+    train_dataset_mfccs = [MFCC(i, sample_rate=16000).feature_vector.T for i in train_dataset[label]]
     local_bar = tqdm(desc="Local Progress", total=len(train_dataset_mfccs), position=0)
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for pred_label in executor.map(mc.predict, train_dataset_mfccs):
@@ -35,7 +36,7 @@ for index, label in enumerate(TI_DIGITS_LABELS):
             local_bar.update()
 
     # Unseen data
-    test_dataset_mfccs = [MFCC(i, sample_rate=16000).feature_vector.T for i in train_dataset[label][10:20]]
+    test_dataset_mfccs = [MFCC(i, sample_rate=16000).feature_vector.T for i in test_dataset[label]]
     local_bar = tqdm(desc="Local Progress", total=len(test_dataset_mfccs), position=0)
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for pred_label in executor.map(mc.predict, test_dataset_mfccs):
@@ -68,8 +69,8 @@ def plot_confusion_matrix(confusion_matrix: NDArray, name: str):
 
 logger.info(f"Confusion matrix when prediction on known data:")
 logger.info(f"{train_data_confusion_matrix}")
-plot_confusion_matrix(train_data_confusion_matrix, "known_data_small_model")
+plot_confusion_matrix(train_data_confusion_matrix, "known_data_big_model")
 logger.info(f"Confusion matrix when prediction on unknown data:")
 logger.info(f"{test_data_confusion_matrix}")
-plot_confusion_matrix(test_data_confusion_matrix, "unknown_data_small_model")
+plot_confusion_matrix(test_data_confusion_matrix, "unknown_data_big_model")
 
